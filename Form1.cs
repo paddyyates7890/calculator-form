0a1ab465340fcb2187f82ef3dfc4a9e20aab5e71
paddyyates7890 <64584807+paddyyates7890@users.noreply.github.com>
2020-06-05 20:56:12 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coolcalculator
+{
+    public partial class Form1 : Form
+    {
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+        String OPERATION;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {                                                           // equals button and operations 
+            num2 = int.Parse(textBox1.Text);
+            
+            if (OPERATION == "+")
+            {
+                num3 = num1 + num2;
+            }
+            else if (OPERATION == "-")
+            {
+                num3 = num1 - num2;
+            }
+            else if (OPERATION == "*")
+            {
+                num3 = num1 * num2;
+            }
+            else
+            {
+                num3 = num1 / num2;
+            }
+
+            textBox1.Text = Convert.ToString(num3);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)    // all number buttons downwards
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "7";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "1";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "0";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "2";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "3";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "4";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)     
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "5";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "6";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "8";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "9";
+            }
+        }                                                               // last number button 
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";                                        // clear button
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Text = "0";
+            OPERATION = "+";  // add button
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Text = "0";
+            OPERATION = "-";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Text = "0";
+            OPERATION = "*";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Text = "0";
+            OPERATION = "/";
+        }
+    }
+}
